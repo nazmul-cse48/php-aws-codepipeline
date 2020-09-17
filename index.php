ce5df8553a4a69abcad2cc3abfcf02115652746e
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2020-09-18 01:35:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 
-This is a Test for DevOps Pipeline tested by Khalid. reviwed by the Khalid it self. Hello from DevSkill DevOps 1st Batch
+This is a Test for DevOps Pipeline tested by Khalid. reviwed by the Khalid it self. Hello from DevSkill DevOps 2nd Batch
