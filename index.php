6610f8b4b03e8837797440443533d63f4be0383a
Khalid Bin Sattar <42968874+khalids226@users.noreply.github.com>
2020-09-18 10:55:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 
-This is a Test for DevOps Pipeline tested by Khalid. reviwed by the Khalid it self. Hello from DevSkill DevOps 2nd Batch
+This is a Test for DevOps Pipeline tested by Khalid. reviwed by the Khalid it self. Hello from DevSkill DevOps 2nd Batch.
+
+This is to certify that the codedeploy is working fine.
+
